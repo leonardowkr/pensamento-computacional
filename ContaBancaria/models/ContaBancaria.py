73908ef73bc8a86239e7d73cd59be4f1f1613b19
Leonardo Wecker <leowecker185@gmail.com>
2025-05-05 21:52:11 -0300

    ContaBancaria

---
+++ ContaBancaria/models/ContaBancaria.py
@@ -0,0 +1,17 @@
+class ContaBancaria: 
+    def __init__(self, titular, saldo, limite, historico):
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+        self.historico = historico
+    
+    def depositar():
+        None
+    def sacar():
+        None
+    def transferir():
+        None
+    def exibir_historico():
+        None
+    def exibir_saldo(): 
+        None
